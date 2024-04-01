391758158d6f6802cefc75eb50af834aa3c3759e
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-01 21:26:57 +0200

    add folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
+
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-800">
+    <h1 className="text-3xl font-bold underline text-red-800">
       Hello world!
     </h1>
     </>
