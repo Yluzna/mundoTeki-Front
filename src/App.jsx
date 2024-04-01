818f1616e90269af1b552a5f80e7a0b17c6548c6
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-01 21:03:49 +0200

    feat: configuration tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-
+      <h1 className="text-3xl font-bold underline text-red-800">
+      Hello world!
+    </h1>
     </>
   )
 }
