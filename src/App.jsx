f15fbe9b6a68db140cf0e0e9e8a7062d543e1150
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-03 15:52:14 +0200

    addess routes in pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import './App.css'
-import NavBar from './components/Navbar/Nav'
-import Categories from './components/Categories/Categories'
-import Footer from './components/Footer/Footer'
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/Navbar/Nav';
+import Categories from './components/Categories/Categories';
+import Footer from './components/Footer/Footer';
 
 function App() {
-
-
-  return (
-    <>
-    <NavBar/>
-    <Categories />
-    <Footer />
-    </>
-  )
+ return (
+    <Router>
+      <NavBar/>
+      <Categories />
+      <Footer />
+    </Router>
+ );
 }
 
-export default App
+export default App;
---
+++ src/page/LatestNews.jsx
@@ -0,0 +1,14 @@
+// LatestNews.js
+import React from 'react';
+
+const LatestNews = () => {
+ return (
+    <div>
+      <h1>Últimas Noticias</h1>
+      <p>Aquí encontrarás las últimas noticias.</p>
+      {/* Aquí puedes agregar más contenido o un botón para navegar a otra página */}
+    </div>
+ );
+};
+
+export default LatestNews;
