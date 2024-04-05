81f8ba73b3ac401247f9cdf0e7fc92f10e19a652
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-05 10:40:04 +0200

    addess page Latesnews

--- src/page/LatestNews.jsx
+++ src/page/LatestNews.jsx
@@ -1,12 +1,11 @@
-// LatestNews.js
+import { redirect } from "react-router-dom";
+import Categories from "../components/Categories/Categories";
 import React from 'react';
 
 const LatestNews = () => {
  return (
     <div>
-      <h1>Últimas Noticias</h1>
-      <p>Aquí encontrarás las últimas noticias.</p>
-      {/* Aquí puedes agregar más contenido o un botón para navegar a otra página */}
+      <Categories />
     </div>
  );
 };
