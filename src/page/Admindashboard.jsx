621ced5bb052c105ef7b43f98757d4b94d918f0e
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-09 09:58:29 +0200

    addess page admin

--- src/page/Admindashboard.jsx
+++ src/page/Admindashboard.jsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import UserCard from './UserCard'; // Asegúrate de ajustar la ruta de importación según la estructura de tu proyecto
+import React from "react";
 
-const AdminPage = () => {
- const [users, setUsers] = useState([]);
-
- useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error('Error fetching data: ', error));
- }, []);
-
+function AdminDashboard() {
  return (
     <div>
-      <h1>Página del Usuario</h1>
-      {users.map(user => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      <h1>Panel de Administrador</h1>
     </div>
  );
-};
+}
 
-export default AdminPage;
+export default AdminDashboard;
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../page/Home";
-// import AdminDashboard from "../page/Admindashboard"
-//import Detailnotice from "../page/Detailnotice"; 
-// import Company
+import AdminDashboard from "../page/AdminDashboard";
+//import Detailnotice from "../page/Detailnotice";
 import App from "../App"
 
 
@@ -11,54 +10,53 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      {
+  /*     {
         path: "/",
-        element:"/login",
-      },
+        element: "/login",
+      }, */
       {
         path: "/home",
         element: <Home />,
       },
-        /* {
-          path: "/admin",
-          element: <AdminDashboard />,
-        }, */
+      {
+        path: "/admin",
+        element: <AdminDashboard />,
+      },
       /* {
         path: "/detailnotice",
         element: <Detail />,
       }, */
-        /* {
-          path: "/newnotice",
-          element: <AddNewNotice />,
-      }, */
-    
-/*     {
-        path: "/company",
-        element: <Company />
+      /* {
+        path: "/newnotice",
+        element: <AddNewNotice />,
     }, */
-     /*  {
-        path: "/science&tecnology",
-        element: <Science />,
-      },
-      ,
-      {
-        path: "/culture&segurity",
-        element: <culture />,
-      },
-      ,
-      {
-        path: "/developmen&programing",
-        element: <developmen />,
-      },
-      {
-        path: "/gaming",
-        element: <gaming />,
-      }, 
-      {
-        path: "/latestnews",
-        element: <latestnews />,
-      }, */
+
+      /*     {
+              path: "/company",
+              element: <Company />
+          }, */
+      /*  {
+         path: "/science&tecnology",
+         element: <Science />,
+       },
+       ,
+       {
+         path: "/culture&segurity",
+         element: <culture />,
+       },
+       ,
+       {
+         path: "/developmen&programing",
+         element: <developmen />,
+       },
+       {
+         path: "/gaming",
+         element: <gaming />,
+       }, 
+       {
+         path: "/latestnews",
+         element: <latestnews />,
+       }, */
     ],
   },
 ]);
- 
