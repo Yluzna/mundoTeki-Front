523c34a7b705872e9ae63d9e20fa7b3d30f9b4c4
Denisse <denisse_na@hotmail.com>
2024-04-17 16:16:03 +0200

    change package

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/Navbar/Nav';
-import Categories from './components/Categories/Categories';
-import Footer from './components/Footer/Footer';
+import './App.css'
+import NavBar from './components/Navbar/Nav'
+import Categories from './components/Categories/Categories'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router'
+
 
 function App() {
   return (
     <>
     <NavBar/>
     <Categories />
+    <Outlet />
     <Footer />
     </>
   )
