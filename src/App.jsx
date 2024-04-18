ce3d2d7ae9069bce1a26d866a597b1f0b12aa77e
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-18 08:52:48 +0200

    acepted changes the merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import Categories from './components/Categories/Categories'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> cbf0d6b87a6a8476e3f71e1585f27b0382b772b0
 
 function App() {
   return (
