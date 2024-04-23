ff0fc0232e5f49b1fbb7463805187b38eb3d4a91
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-23 13:12:57 +0200

    uncomment in layoutadmin

--- src/components/LayoutAdmin/LayoutAdmin.jsx
+++ src/components/LayoutAdmin/LayoutAdmin.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-//import Sidebar from "../Admin/SideBar/SideBar";
-//import Header from "../Admin/Header/Header";
+import Sidebar from "../Admin/SideBar/SideBar";
+import Header from "../Admin/Header/Header";
 
 function LayoutAdmin() {
   return (
     <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
-        {/* {*<Sidebar />*} */}
+      <Sidebar />
       <div className="xl:col-span-5 p-8">
-        {/* <Header /> */}
+        <Header />
         <div className="h[90vh] overflow-y-scroll">
-        <Outlet />
+          <Outlet />
         </div>
       </div>
     </div>
