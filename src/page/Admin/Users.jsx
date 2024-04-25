047ffc72f2d3e0af1bf7c7eea1afd36b293950f4
Yeshiprinss <martinezyessenia068@gmail.com>
2024-04-25 17:11:04 +0200

    add component Searchbar, style in page of admin

--- src/page/Admin/Users.jsx
+++ src/page/Admin/Users.jsx
@@ -1,12 +1,13 @@
 import UserTitle from "../../components/Admin/User/UserTitle";
 import UsersTable from "../../components/Admin/User/UsersTable";
-
+import SearchBar from "../../components/Admin/SearchBar/SearchBar";
 
 
 const Users = () => {
     return (
 <>
 <UserTitle />
+<SearchBar />
 <UsersTable />
 </>
     );
